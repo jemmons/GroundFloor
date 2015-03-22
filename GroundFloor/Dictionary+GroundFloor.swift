c1b784ea45eb79a7684e2f9e7451cfd62a119022
Joshua Emmons <joshua@reverb.com>
2015-03-22 11:55:49 -0500

    Overrides "+" operator for Dictionary
    Also constrains the `Key` generic type to be `Hashable`.

--- GroundFloor/Dictionary+GroundFloor.swift
+++ GroundFloor/Dictionary+GroundFloor.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 
-public func merge<Key,Value>(target:[Key:Value], source:[Key:Value])->[Key:Value]{
+public func merge<Key:Hashable,Value>(target:[Key:Value], source:[Key:Value])->[Key:Value]{
   return reduce(source, target){ (var last, this) in
     let (key, value) = this
     last.updateValue(value, forKey:key)
@@ -9,3 +9,7 @@ public func merge<Key,Value>(target:[Key:Value], source:[Key:Value])->[Key:Value
   }
 }
 
+
+public func +<Key:Hashable,Value>(lhs:[Key:Value], rhs:[Key:Value]) -> [Key:Value]{
+  return merge(lhs, rhs)
+}
--- GroundFloorTests/DictionaryTests.swift
+++ GroundFloorTests/DictionaryTests.swift
@@ -6,6 +6,20 @@ class DictionaryTests: XCTestCase {
   func testMerge() {
     XCTAssertEqual(merge(["one": 1, "two": 2, "three":3], ["three":42, "four":4, "five":5]), ["one":1, "two":2, "three":42, "four":4, "five":5])
   }
+
+  func testMergeOperator() {
+    XCTAssertEqual(["one": 1, "two": 2, "three":3] + ["three":42, "four":4, "five":5], ["one":1, "two":2, "three":42, "four":4, "five":5])
+  }
+  
+  func testImmutability(){
+    let target = ["one":1]
+    let source = ["two":2]
+    let added = target + source
+    let merged = merge(target, source)
+    //Pedantic clarity. Becasue these are constant, we don't really need to assert anything (the compiler will barf if we try to mutate them).
+    XCTAssertEqual(target, ["one":1])
+    XCTAssertEqual(source, ["two":2])
+  }
 }
 
 
